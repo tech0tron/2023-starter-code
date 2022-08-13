a4b84316eab4731c3b3c6c632e9e95fa03060457
Tech0tron <tech0tron.roblox@gmail.com>
2022-08-13 13:47:02 -0600

    What the code looked like at 1:46pm

--- src/main/kotlin/frc/robot/commands/Drive.kt
+++ src/main/kotlin/frc/robot/commands/Drive.kt
@@ -1,7 +1,12 @@
 package frc.robot.commands
 
+import edu.wpi.first.math.controller.PIDController
+import edu.wpi.first.networktables.NetworkTable
+import edu.wpi.first.networktables.NetworkTableInstance
 import edu.wpi.first.wpilibj2.command.CommandBase
+import edu.wpi.first.wpilibj2.command.PIDCommand
 import frc.robot.subsystems.Drivetrain
+import java.time.LocalTime
 
 /** Now that you've written the Drivetrain subsystem (or maybe you're doing this first idk), it's time to write
  * the command to drive the robot.
@@ -12,25 +17,41 @@ class Drive(val drivetrain: Drivetrain, val speeds: () -> Pair<Double, Double>
      */
 ) : CommandBase() {
 
-    /** Like the DrivetrainSubsystem, are there any useful variables we should initialize?**/
+    // testing values!
+    var time: Long = 0
+    var lastTime: Long = 0
+    var deltaTime: Double = 0.0
+    var measured = 0.0
+    var setpoint = 0.0
 
+
+    var entry = NetworkTableInstance.getDefault().getTable("PID Tuning").getEntry("Drivetrain")
+    var text = "time,measured,setpoint"
+    /** Like the DrivetrainSubsystem, are there any useful variables we should initialize?**/
+    val leftPID = PIDController(1.0,0.0,0.0)
+    val rightPID = PIDController(1.0,0.0,0.0)
     init {
         /** This line here is required by WPILib, it just tells the CommandScheduler (which runs things behind
             the scenes what subsystems this command is using.
          **/
         addRequirements(drivetrain)
-
+        entry.setString(text)
         /** Feel free to put any init code you think would be useful **/
     }
 
     override fun execute() {
         super.execute()
-        var speedsToSet = speeds()
-        drivetrain.tankDriveVolts(speedsToSet.first, speedsToSet.second)
-        /** Here you can put code that will be run periodicaly (20 times/second)
-         * Here is a good place to put the code that will read in whatever inputs you're using
-         * and apply the new voltage or speed to the motors.
-         */
+        var desired = speeds()
+        var measured = drivetrain.wheelSpeeds()
+
+        var leftPIDGain = leftPID.calculate(measured.leftMetersPerSecond, desired.first)
+        var rightPIDGain = rightPID.calculate(measured.rightMetersPerSecond, desired.second)
+        drivetrain.tankDriveVolts(desired.first, desired.second)
+        lastTime = time
+        time = System.currentTimeMillis()
+        deltaTime = (time - lastTime).toDouble() / 1000.0
+        text += "\n${time},${measured.leftMetersPerSecond},${desired.first}"
+        entry.setString(text)
     }
 
     override fun end(interrupted: Boolean) {
--- src/main/kotlin/frc/robot/subsystems/Drivetrain.kt
+++ src/main/kotlin/frc/robot/subsystems/Drivetrain.kt
@@ -2,6 +2,10 @@ package frc.robot.subsystems
 
 import com.revrobotics.CANSparkMax
 import com.revrobotics.CANSparkMaxLowLevel
+import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds
+import edu.wpi.first.math.util.Units
+import edu.wpi.first.wpilibj.drive.DifferentialDrive
+import edu.wpi.first.wpilibj.drive.MecanumDrive.WheelSpeeds
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 
@@ -13,6 +17,7 @@ class Drivetrain(
          * What do you think we should pass into the drivetrain subystem?
          * Motors? Controllers? Constants? Anything else? **/
 ) : SubsystemBase() {
+
     val motorLF = CANSparkMax(1, CANSparkMaxLowLevel.MotorType.kBrushless)
     val motorRF = CANSparkMax(3, CANSparkMaxLowLevel.MotorType.kBrushless)
     val motorLB = CANSparkMax(2, CANSparkMaxLowLevel.MotorType.kBrushless)
@@ -21,18 +26,16 @@ class Drivetrain(
     val motorsRight = MotorControllerGroup(motorRF, motorRB)
     val motorsLeft = MotorControllerGroup(motorLF, motorLB)
 
-
-    /**
-     * Here you can specify any variables you think would be helpful for the class.
-     * Last year we had motorcontrollergroups and encoders initialized here.
-     */
+    val encoderLF = motorLF.encoder
+    val encoderRF = motorRF.encoder
+    val encoderLB = motorLB.encoder
+    val encoderRB = motorRB.encoder
 
     init {
         /** The code you write in here will be run as soon as a Drivetrain instance is created.
          * What do you think should go here?
          */
         motorsRight.inverted = true
-
     }
 
     override fun periodic() {
@@ -50,5 +53,12 @@ class Drivetrain(
     fun tankDriveVolts(leftVolts : Double, rightVolts : Double){
         motorsLeft.setVoltage(leftVolts)
         motorsRight.setVoltage(rightVolts)
+    }
+
+    fun wheelSpeeds(): DifferentialDriveWheelSpeeds {
+        return DifferentialDriveWheelSpeeds(
+            (encoderLF.velocity / 10.71) * (Math.PI * Units.inchesToMeters(6.0)) / 60,
+            (encoderRF.velocity / 10.71) * (Math.PI * Units.inchesToMeters(6.0)) / 60
+        )
     }
 }
